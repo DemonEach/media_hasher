3d7c3677f81021295385f14c68b3615bea7e5485
DemonEach <65486833+DemonEach@users.noreply.github.com>
2020-06-28 14:46:34 +0500

    Added files

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+extern crate crypto;
+
+use crypto::digest::Digest;
+use crypto::md5::Md5;
+use std::env::current_dir;
+use std::fs;
+use std::fs::rename;
+use std::io::Read;
+use std::path::PathBuf;
+
+fn main() -> Result<(), std::io::Error> {
+    let curr_dir: PathBuf = current_dir().unwrap();
+    println!("Current dir: {}", curr_dir.display());
+    let paths = get_img_files(curr_dir);
+    rename_files_with_hash(paths);
+    Ok(())
+}
+
+fn get_img_files(curr_dir_path: PathBuf) -> Vec<PathBuf> {
+    let paths = fs::read_dir(curr_dir_path).unwrap();
+
+    let mut result_files: Vec<PathBuf> = vec![];
+    println!("_______________________");
+    println!("Found this image files:");
+    for entry in paths {
+        if let Ok(entry) = entry {
+            if is_img(&entry.path()) {
+                result_files.push(entry.path());
+                println!("{}", entry.path().display());
+            }
+        }
+    }
+    println!("_______________________");
+    return result_files;
+}
+
+fn get_file_ext(path: &PathBuf) -> String {
+    return path.extension().unwrap().to_str().unwrap().to_owned();
+}
+
+fn is_img(path: &PathBuf) -> bool {
+    if path.is_file() {
+        let ext = get_file_ext(&path);
+        match ext.as_str() {
+            "jpg" | "jpeg" | "gif" | "bmp" | "png" | "webp" => return true,
+            _ => return false,
+        };
+    }
+    return false;
+}
+
+fn rename_files_with_hash(paths: Vec<PathBuf>) {
+    for path in paths {
+        let mut file = fs::File::open(&path).unwrap();
+        let file_buff: &mut [u8] = &mut [];
+        let mut hasher = Md5::new();
+        match file.read(file_buff) {
+            Ok(_res) => {}
+            Err(_err) => panic!("Cannot read file {}", &path.display()),
+        };
+        hasher.input(file_buff);
+        let hash: String = hasher.result_str();
+        let new_file_name = format!("{}.{}", hash, get_file_ext(&path));
+        match rename(&path, &new_file_name) {
+            Ok(_val) => println!(
+                "Successfully renamed file\n {} to {:?}",
+                &path.display(),
+                &new_file_name
+            ),
+            Err(err) => panic!(
+                "Cannot rename file {}, caused error {}",
+                &path.display(),
+                err
+            ),
+        };
+    }
+}
